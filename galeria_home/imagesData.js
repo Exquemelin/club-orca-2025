61c8ae3cd670c5136e409a28eb150e4df95a48e4
Exquemelin <63191395+Exquemelin@users.noreply.github.com>
2025-01-04 22:39:55 +0100

    First version

---
+++ galeria_home/imagesData.js
@@ -0,0 +1,69 @@
+export const imageData = [
+{
+    path: './assets/galery/galeria_home/01_red.jpg',
+    alt:'01_red', 
+    title:'01_red',
+},
+{
+    path: './assets/galery/galeria_home/02_red.jpg',
+    alt:'02_red', 
+    title:'02_red',
+},
+{
+    path: './assets/galery/galeria_home/03_red.jpg',
+    alt:'03_red', 
+    title:'03_red',
+},
+{
+    path: './assets/galery/galeria_home/04_red.jpg',
+    alt:'04_red', 
+    title:'04_red',
+},
+{
+    path: './assets/galery/galeria_home/05_red.jpg',
+    alt:'05_red', 
+    title:'05_red',
+},
+{
+    path: './assets/galery/galeria_home/06_red.jpg',
+    alt:'06_red', 
+    title:'06_red',
+},
+{
+    path: './assets/galery/galeria_home/07_red.jpg',
+    alt:'07_red', 
+    title:'07_red',
+},
+{
+    path: './assets/galery/galeria_home/08_red.jpg',
+    alt:'08_red', 
+    title:'08_red',
+},
+]
+
+export const training = [
+    {
+        src: './assets/images/home_formacion_bje.png',
+        alt: 'Bautismos',
+        title: 'Bautismos',
+        text: 'BAUTISMOS',
+    },
+    {
+        src: './assets/images/home_formacion_guia.png',
+        alt: 'Formación Buceador',
+        title: 'Formación Buceador',
+        text: 'CURSOS DE BUCEO',
+    },
+    {
+        src: './assets/images/home_formacion_b1e.png',
+        alt: 'Formación Especialidades',
+        title: 'Formación Especialidades',
+        text: 'ESPECIALIDADES',
+    },
+    {
+        src: './assets/images/home_formacion_especialidad.png',
+        alt: 'Formación guías de grupo',
+        title: 'Formación guías de grupo',
+        text: 'CURSO DE GUÍA',
+    },
+]
